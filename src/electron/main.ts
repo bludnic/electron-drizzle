8cdab3f9192e0fa77ab26468818a20a9cf5ecadc
bludnic <victorcpla@gmail.com>
2025-08-29 00:25:27 +0100

    chore: configure Electron

---
+++ src/electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.mjs"),
+      // ESM preload scripts must be context isolated to use dynamic Node.js ESM imports
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,52 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: {
-    main: "./src/main.ts",
+export default defineConfig([
+  // Electron main.js
+  {
+    entry: { main: "./src/electron/main.ts" },
+    format: "esm",
+    outDir: "dist",
+    clean: true,
+    target: "esnext",
+    treeshake: true,
+    external: ["electron"], // Don't bundle Electron
+    outExtension: ({ format }) => {
+      if (format === "esm") return { js: ".mjs" };
+      if (format === "cjs") return { js: ".cjs" };
+      return { js: ".js" };
+    },
+    esbuildOptions: (options, ctx) => {
+      // Polyfill Node globals in ESM
+      options.banner = {
+        js: `
+          import { createRequire } from 'module';
+
+          const require = createRequire(import.meta.url);
+
+          if (typeof globalThis.__dirname === "undefined") {
+            globalThis.__dirname = new URL('.', import.meta.url).pathname;
+          }
+          if (typeof globalThis.__filename === "undefined") {
+            globalThis.__filename = new URL(import.meta.url).pathname;
+          }
+        `,
+      };
+    },
   },
-  format: "esm",
-  outDir: "dist",
-});
+
+  // Electron preload.js
+  {
+    entry: { preload: "./src/electron/preload.ts" },
+    format: "esm",
+    outDir: "dist",
+    clean: true,
+    target: "esnext",
+    treeshake: true,
+    external: ["electron"], // Don't bundle Electron
+    outExtension: ({ format }) => {
+      if (format === "esm") return { js: ".mjs" };
+      if (format === "cjs") return { js: ".cjs" };
+      return { js: ".js" };
+    },
+  },
+]);
